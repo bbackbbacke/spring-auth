ecf47e5480681981db263e3919ec14d17456e217
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-05-28 04:27:04 +0900

    Spring Security 로그인

--- src/main/java/com/sparta/springauth/controller/ProductController.java
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.sparta.springauth.controller;
 
 import com.sparta.springauth.entity.User;
+import com.sparta.springauth.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductController {
 
     @GetMapping("/products")
-    public String getProducts(HttpServletRequest req) {
-        System.out.println("ProductController.getProducts : 인증 완료");
-        User user = (User) req.getAttribute("user");
+    public String getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        //Authentication의 Principle에 들어가있는 UserDetails가지고 오는것! (SecurityContextHolder 내의 SecurityContext내의 Authentication내의 principle, credentials, authorities  중 principle 내의 UserDetails)
+        User user = userDetails.getUser();
         System.out.println("user.getUsername() = " + user.getUsername());
 
         return "redirect:/";
