1d90663321b87919740e90ab297fa5ff77c7361b
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-05-27 17:57:59 +0900

    회원가입 구현

---
+++ src/main/java/com/sparta/springauth/entity/User.java
@@ -0,0 +1,38 @@
+package com.sparta.springauth.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users") //이름 따로 지정하면 테이블이 user가 아닌 users로(지정한 이름으로) 만들어짐
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING) //type String이므로 enum이름 값을 db에 저장 ( USER -> db : USER
+    private UserRoleEnum role;
+
+    public User(String username, String password, String email, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+
+    }
+}
---
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -0,0 +1,59 @@
+package com.sparta.springauth.service;
+
+import com.sparta.springauth.dto.SignupRequestDto;
+import com.sparta.springauth.entity.User;
+import com.sparta.springauth.entity.UserRoleEnum;
+import com.sparta.springauth.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    // ADMIN_TOKEN
+    private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    public void signup(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        // 1. 회원 중복 확인
+        Optional<User> checkUsername = userRepository.findByUsername(username); //Optional은 NPE 발생을 방지하기 위해 사용합니다. 메서드가 반환할 결과값이 ‘없음’을 명백하게 표현할 필요가 있고, null을 반환하면 에러를 유발할 가능성이 높은 상황에서 메서드의 반환 타입으로 Optional을 사용하자는 것이 Optional을 만든 주된 목적입니다.
+        //isPresent - 현재 Optional에 넣어준 '값'이 있는지 없는지 확인 해주는 메서드
+        if (checkUsername.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+
+        // 2. email 중복확인
+        String email = requestDto.getEmail();
+        Optional<User> checkEmail = userRepository.findByEmail(email);
+        if (checkEmail.isPresent()) {
+            throw new IllegalArgumentException("중복된 Email 입니다.");
+        }
+
+        // 3. 사용자 ROLE 확인
+        UserRoleEnum role = UserRoleEnum.USER;
+        //boolean타입의 변수는 is로 시작(isAdmin = requestDto에서 boolean타입의 Admin을 가지고옴)
+        if (requestDto.isAdmin()) {
+            if (!ADMIN_TOKEN.equals(requestDto.getAdminToken())) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        // 사용자 등록
+        // **JPA** db의 한 줄(하나의 row)은 데이터베이스에 해당하는 entity Class에 하나의 객체!
+        User user = new User(username, password, email, role);
+        userRepository.save(user);
+    }
+}
